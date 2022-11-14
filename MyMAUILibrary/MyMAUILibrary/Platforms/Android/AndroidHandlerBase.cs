f2ee9b5e1f73d9b940e353bac875f089a5dfbac6
temamarvel <tema.denisoff@gmail.com>
2022-11-14 23:25:22 +0400

    add core lib

---
+++ MyMAUILibrary/MyMAUILibrary/Platforms/Android/AndroidHandlerBase.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Maui.Handlers;
+
+namespace MyMAUILibrary;
+
+//just for compile and symmetry of the architecture
+public abstract class
+    AndroidHandlerBase<TVirtualView, TPlatformView> : ViewHandler<TVirtualView, TPlatformView>
+    where TVirtualView : class, IView
+    where TPlatformView : Android.Views.View {
+    protected AndroidHandlerBase([NotNull] IPropertyMapper mapper, CommandMapper commandMapper = null) : base(mapper, commandMapper) {
+    }
+}
+
---
+++ MyMAUILibrary/MyMAUILibrary/Platforms/iOS/iOSHandlerBase.cs
@@ -0,0 +1,28 @@
+﻿using Bindings.SwiftUIToMaui;
+using Foundation;
+using Microsoft.Maui.Handlers;
+using UIKit;
+
+namespace MyMAUILibrary;
+
+public abstract class iOSHandlerBase<TWrapper, TVirtualView, TPlatformView> : ViewHandler<TVirtualView, TPlatformView>
+    where TVirtualView : class, IView
+    where TPlatformView : UIView
+    where TWrapper : NSObject, IUIViewHost, new()
+{
+    protected TWrapper Wrapper { get; set; }
+
+    protected iOSHandlerBase(IPropertyMapper propertyMapper) : base(propertyMapper) { }
+    
+    
+    // public static IPropertyMapper<TVirtualView, MyHandlerBase<TWrapper, TVirtualView, TPlatformView>> PropertyMapper =
+    //     new PropertyMapper<TVirtualView, MyHandlerBase<TWrapper, TVirtualView, TPlatformView>>(ViewHandler.ViewMapper)
+    //     {
+    //     };
+    
+    protected override TPlatformView CreatePlatformView() {
+        Wrapper = new TWrapper();
+        return (TPlatformView)Wrapper.UiView;
+    }
+}
+
