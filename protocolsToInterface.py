2624bccea152e819ca1b8fc6de26729c0c146d6b
temamarvel <tema.denisoff@gmail.com>
2022-11-15 11:40:53 +0400

    small interface fixer script refactoring

--- protocolsToInterface.py
+++ protocolsToInterface.py
@@ -5,16 +5,17 @@ protocolNames = []
 fileData = ""
 
 with open('ApiDefinitions.cs') as f:
-    protocolNames = [line for line in f.readlines() if "// @protocol " in line]
+    protocolNames = [line for line in f.readlines() if protocol_srting in line]
     
 with open('ApiDefinitions.cs') as f:
     fileData = f.read()
 
 for line in protocolNames:
     line.strip()
-    interfaceName = line[line.find(protocol_srting) + len(protocol_srting) + 1:]
-    print("replace for: I" + interfaceName)
-    fileData = fileData.replace("I" + interfaceName, interfaceName)
+    protocolName = line[line.find(protocol_srting) + len(protocol_srting) + 1:]
+    interfaceName = "I" + line[line.find(protocol_srting) + len(protocol_srting) + 1:]
+    print("replace for: " + interfaceName)
+    fileData = fileData.replace(interfaceName, protocolName)
 
 with open('ApiDefinitions.cs', "w") as f:
     f.write(fileData)
